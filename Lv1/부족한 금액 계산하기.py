66f9dc5ccce04c47f171b316492a437e09b85b82
jiminkyung <jimiiwhatttttttttt@gmail.com>
2024-03-15 17:29:06 +0900

    240315

--- Lv1/부족한 금액 계산하기.py
+++ Lv1/부족한 금액 계산하기.py
@@ -2,3 +2,7 @@ def solution(price, money, count):
     cost = sum(i for i in range(price, price*count+1, price))
     ret = money - cost
     return -(ret) if ret < 0 else 0
+
+# 파이써닉한 코드.
+def solution(price, money, count):
+    return max(0,price*(count+1)*count//2-money)
