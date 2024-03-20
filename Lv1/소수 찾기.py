79a607c8671fde8cf38be72eb8888f4858b39402
jiminkyung <jimiiwhatttttttttt@gmail.com>
2024-03-20 19:51:42 +0900

    240320

--- Lv1/소수 찾기.py
+++ Lv1/소수 찾기.py
@@ -19,3 +19,20 @@ def solution(n):
         if i in num:
             num-=set(range(2*i,n+1,i))
     return len(num)
+
+# GPT에게 더 효율적인 코드를 물어본 결과
+def solution(n):
+    if n < 2:  # 2 미만의 경우 소수가 없으므로 0을 반환
+        return 0
+    
+    # 초기에 모든 수를 소수로 가정(0과 1 제외)
+    prime = [True] * (n+1)
+    prime[0], prime[1] = False, False  # 0과 1은 소수가 아님
+
+    m = int(n**0.5)  # 검사할 최대 수의 제곱근 계산
+    for i in range(2, m+1):
+        if prime[i]:  # i가 소수인 경우
+            for j in range(i*i, n+1, i):  # i의 배수들을 제거
+                prime[j] = False
+    
+    return sum(prime)  # True(소수)의 개수 반환
