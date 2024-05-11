90920eaf64defd2550324c0453e493cc130d34d0
지민경 <jimiiwhatttttttttt@gmail.com>
2024-05-11 17:31:49 +0900

    🎨 두번째 풀이 수정
    - "0"을 더해주는경우 무조건 "01"이 나오게 됨. 따라서 `rfind`의 결과가 -1인 경우는 없음.
    - 설명 오류 수정. 1로만 이루어진 수일경우, 1000...으로 변경하는것이 아님.

--- Programmers/Lv2/2개 이하로 다른 비트.py
+++ Programmers/Lv2/2개 이하로 다른 비트.py
@@ -3,9 +3,8 @@
 첫번째 풀이는 내 풀이인데, 무식하게 하나하나 계산하는 방식이다.
 
 두번째 풀이는 짝수일경우 맨 마지막비트를 1로 변환 => 홀수,
-홀수일경우 01인 부분을 찾아 10으로 변환해준다.
-만약 01인 부분이 없다면 전체가 1이라는 소리이므로 1 + 길이만큼0 을 반환한다.
-ex) 13 -> 1101 -> 1110(14), 15 -> 1111 -> 10000(16)
+홀수일경우 맨 앞에 0을 추가해준 뒤, 01인 부분을 찾아 10으로 변환해준다.(1로만 이루어진 수일 경우를 대비)
+ex) 13 -> 1101 -> 1110(14), 15 -> 1111 -> 10111(23)
 """
 
 # 첫번째 풀이. TC 10, 11 시간초과.
@@ -29,11 +28,6 @@ def solution(numbers):
         else:
             binary = "0" + bin(number)[2:]
             idx = binary.rfind("01")
-            if idx == -1:
-                binary = "10" + binary[1:].replace("1", "0")
-            else:
-                binary = binary[:idx] + "10" + binary[idx+2:]
+            binary = binary[:idx] + "10" + binary[idx+2:]
             ret.append(int(binary, 2))
     return ret
-
-print(solution([2, 7]))
