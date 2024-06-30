be2db77aaedd09e9f22a5d8fa010ec638c3b4a7f
지민경 <jimiiwhatttttttttt@gmail.com>
2024-06-30 21:51:11 +0900

    240630

---
+++ Backjoon/25192_인사성 밝은 곰곰이.py
@@ -0,0 +1,35 @@
+# 심화 2
+# 메모리: 43596KB / 시간: 72ms
+
+from sys import stdin
+
+
+logs = iter(stdin.read().split())
+next(logs)
+enter = ret = 0
+curr = set()
+
+for log in logs:
+    if log == "ENTER":
+        enter += 1
+        curr = set()
+        continue
+
+    if log in curr:
+        continue
+    else:
+        curr.add(log)
+        ret += 1
+
+print(ret)
+
+
+# 더 간단한 방법.
+# 메모리: 44548KB / 시간: 60ms
+from sys import stdin
+
+
+logs = iter(stdin.read().split("ENTER"))
+
+ret = sum(len(set(log.split())) for log in logs) - 1
+print(ret)
