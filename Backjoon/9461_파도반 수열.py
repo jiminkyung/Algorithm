2393631b9db3f308e388076c61069f988106e841
지민경 <jimiiwhatttttttttt@gmail.com>
2024-07-10 20:28:00 +0900

    240710
    - 메모리, 실행시간의 차이 요인

--- Backjoon/9461_파도반 수열.py
+++ Backjoon/9461_파도반 수열.py
@@ -32,3 +32,42 @@ for number in data:
 11= 12
 => F(N) = F(N-2) + F(N-3)
 """
+
+
+# 실행시간 더 줄여보기...
+# 메모리: 31120KB / 시간: 36ms
+import sys
+
+
+N = int(sys.stdin.readline())
+
+dp = [0, 1, 1, 1, 2] + [0]*96
+
+for i in range(5, 101):
+    dp[i] = dp[i-2] + dp[i-3]
+
+for _ in range(N):
+    n = int(sys.stdin.readline())
+    sys.stdout.write(f"{dp[n]}\n")
+
+
+# 실행시간 32 찍을때까지.
+# 메모리: 31120KB / 시간: 32ms
+from sys import stdin
+
+
+input = stdin.readline
+N = int(input())
+
+dp = [0, 1, 1, 1, 2] + [0]*96
+
+for i in range(5, 101):
+    dp[i] = dp[i-2] + dp[i-3]
+
+for _ in range(N):
+    n = int(input())
+    print(dp[n])
+
+# 메모리, 실행시간 차이?
+    # => 입/출력처리가 원인일 수 있음.
+    # input = stdin.readline 으로 할당하고, stdout 대신 print()를 사용하는게 더 빠를 수도 있다.
