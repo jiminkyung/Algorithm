7d4ce8643116ddf0f758a7f346beff51f4700100
지민경 <jimiiwhatttttttttt@gmail.com>
2024-07-15 19:14:17 +0900

    💡 240715
    - LIS 문제

--- Backjoon/2565_전깃줄.py
+++ Backjoon/2565_전깃줄.py
@@ -24,3 +24,22 @@ def checking_wires():
     return max(dp)
 
 print(N - checking_wires())
+
+
+# 비슷하지만 실행시간이 32ms인 코드. 리스트 분리하는것보다 이게 더 깔끔한듯.
+n = int(input())
+lines = []
+dp = [1] * n
+
+for i in range(n):
+    a, b = map(int, input().split())
+    lines.append([a, b])
+
+lines.sort()
+
+for i in range(1, n):
+    for j in range(0, i):
+        if lines[j][1] < lines[i][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
