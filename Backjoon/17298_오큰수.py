a7fec5a64009ff22ee875c516d8d2c66c6b8dfb4
지민경 <jimiiwhatttttttttt@gmail.com>
2024-08-04 14:49:17 +0900

    💡 240804

--- Backjoon/17298_오큰수.py
+++ Backjoon/17298_오큰수.py
@@ -14,7 +14,7 @@ ret = [-1] * N
 stack = [0]
 
 for i in range(1, N):
-    while stack and A[stack[-1]] < A[i]:  # 스택이 존재하고 A[스택의 마지막 값(인덱스)]가 현재 A[현재 인덱스] 보다 클때,
+    while stack and A[stack[-1]] < A[i]:  # 스택이 존재하고 A[스택의 마지막 값(인덱스)]보다 현재 A[현재 인덱스]가 클때,
         ret[stack.pop()] = A[i]           # ret[인덱스]를 A[현재 인덱스]값으로 변경.
     stack.append(i)                       # 조건의 참거짓과 상관없이 현재 인덱스값을 스택에 추가.
 
