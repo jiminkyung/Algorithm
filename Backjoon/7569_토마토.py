9c23188cc2d9d93db879b49f6bebef6b75dadd12
지민경 <jimiiwhatttttttttt@gmail.com>
2024-08-13 22:01:07 +0900

    240813
    - 3차원 BFS 문제

---
+++ Backjoon/7569_토마토.py
@@ -0,0 +1,39 @@
+# 그래프와 순회
+
+# 메모리: 48492KB / 시간: 1552ms
+
+from sys import stdin
+from collections import deque
+
+
+input = stdin.readline
+M, N, H = map(int, input().split())  # M: 열, N: 행, H: 높이
+box = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+tomato = deque([])
+
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if box[i][j][k] == 1:
+                tomato.append((i, j, k))
+
+directions = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
+
+def bfs():
+    while tomato:
+        x, y, z = tomato.popleft()
+        for dx, dy, dz in directions:
+            nx, ny, nz = x + dx, y + dy, z + dz
+            if 0 <= nx < H and 0 <= ny < N and 0 <= nz < M and box[nx][ny][nz] == 0:
+                box[nx][ny][nz] = box[x][y][z] + 1
+                tomato.append((nx, ny, nz))
+    
+    ret = float("-inf")
+    for height in box:
+        for row in height:
+            if 0 in row:
+                return -1
+            ret = max(ret, max(row))
+    return ret - 1
+
+print(bfs())
