774c12eec2749c4ad3756c94370291ce6366ed05
지민경 <jimiiwhatttttttttt@gmail.com>
2024-08-26 18:47:57 +0900

    240826
    - MITM(Meet In The Middle) 알고리즘 문제
    - 브루트포스, 이분탐색 알고리즘 사용

--- Backjoon/1450_냅색문제.py
+++ Backjoon/1450_냅색문제.py
@@ -46,3 +46,49 @@ for l in left_sum:
     ret += binary_search(right_sum, C - l, 0, len(right_sum))  # 가능하다면, 정렬된 right_sum에서 C-l이하인 값의 수를 찾는다.
 
 print(ret)
+
+
+# 조합, 이분탐색 모듈을 사용한 풀이.
+from itertools import combinations
+from bisect import bisect_right 
+
+# array 넣으면 부분집합 구하고 부분집합의 합의 list 반환하는 함수
+# 두 부분에 대해서 가능한 모든 부분집합의 무게 합 구하기
+def split_list(lst):
+    result = []
+
+    for i in range(len(lst) + 1): 
+        for comb in combinations(lst, i):
+            result.append(sum(comb))
+
+    return result
+
+
+def solution():
+
+    # 입력 받기
+    N, max_weight = list(map(int, input().split()))
+    weights = list(map(int, input().split()))
+
+    # 물건 리스트 두 부분으로 나누기
+    left_weights = weights[:N//2]
+    right_weights = weights[N//2:]
+
+    left_weights = split_list(left_weights)
+    right_weights = split_list(right_weights)
+
+    # 오른쪽 배열은 이진탐색(bisect)을 위해서 정렬돼있어야 함
+    right_weights.sort()
+
+    # 왼쪽 부분집합의 합을 하나씩 탐색하면서
+    count = 0
+    
+    for left in left_weights:
+        if left <= max_weight:
+            # 오른쪽 부분집합에서 c를 넘지 않는 최대 가능한 값 찾기
+            count += bisect_right(right_weights, max_weight - left)
+    
+    return count
+
+    
+print(solution())
