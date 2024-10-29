5ca0402f6b097a127e4ba6ad1a4557ce6b7e5da2
지민경 <jimiiwhatttttttttt@gmail.com>
2024-10-29 20:22:17 +0900

    💡 241029

--- Backjoon/1475_방 번호.py
+++ Backjoon/1475_방 번호.py
@@ -14,3 +14,26 @@ for p in plastic:
 
 number[9] = number[9] // 2 + number[9] % 2
 print(max(number))
+
+
+# 실행시간, 메모리 1위인 코드.
+# 6과 9를 균등하게 분배한다.
+import sys
+
+
+def main(n):
+    arr = [0 for i in range(10)]
+
+    for x in list(str(n)):
+        arr[int(x)] += 1
+
+    t69 = arr[6] + arr[9]
+    arr[6] = t69 // 2
+    arr[9] = t69 - arr[6]
+
+    return max(arr)
+
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    print(main(n))
