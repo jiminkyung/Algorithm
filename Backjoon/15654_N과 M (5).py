ff70b078d1498899d5a670c29a2fcea2846b238a
지민경 <jimiiwhatttttttttt@gmail.com>
2024-11-07 23:52:25 +0900

    241107

---
+++ Backjoon/15654_N과 M (5).py
@@ -0,0 +1,26 @@
+# 문제집 - 0x0C강 - 백트래킹
+
+
+# 문제: https://www.acmicpc.net/problem/15654
+# 메모리: 31120KB / 시간: 152ms
+from sys import stdin
+
+
+input = stdin.readline
+
+N, M = map(int, input().split())
+nums = sorted(map(int, input().split()))
+
+def backtrack(ret):
+    if len(ret) == M:
+        print(" ".join(map(str, ret)))
+        return
+    
+    for i in range(N):
+        if nums[i] in ret:
+            continue
+        ret.append(nums[i])
+        backtrack(ret)
+        ret.pop()
+
+backtrack([])
