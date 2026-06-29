fa3427cfcb50c88b3c80208699aa0964d5ae4272
지민경 <jimiiwhatttttttttt@gmail.com>
2026-06-29 19:35:33 +0900

    260629
    - 코드 추가

--- Programmers/Lv2/타켓 넘버.py
+++ Programmers/Lv2/타켓 넘버.py
@@ -1,6 +1,32 @@
 # 문제: https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
 # BFS / DFS 모두 가능함.
+# 1) DFS + 메모이제이션
+from sys import stdin
+
+
+input = stdin.readline
+
+def solution(numbers: list[int], target: int):
+    memo = {}
+    N = len(numbers)
+
+    def dfs(idx: int, curr: int):
+        if idx == N:
+            return int(curr == target)
+
+        if (idx, curr) in memo:
+            return memo[(idx, curr)]
+
+        ret = dfs(idx + 1, curr + numbers[idx])+ dfs(idx + 1, curr - numbers[idx])
+
+        memo[(idx, curr)] = ret
+        return ret
+
+    return dfs(0, 0)
+
+
+# 2) DFS만
 from sys import stdin
 
 
